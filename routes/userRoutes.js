4e2d9c6499487b939f07c545f8dfe355be462a99
Asad3601 <asadrehmanrajar1286@gmail.com>
2024-10-20 16:21:46 +0500

    add route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const UserController = require('../controllers/UserController');
 
+router.get('/', UserController.GetUsers);
 router.post('/create-user', UserController.upload.single('image'), UserController.CreateUser);
-router.get('/get-users', UserController.GetUsers);
 
 
 module.exports = router;
